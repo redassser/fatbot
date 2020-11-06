f55afcc8288b1c0bbaf2f8e4981ddf5ae62c5810
redassser <redryan890@gmail.com>
2020-11-06 16:53:19 -0500

    auto vc fix

--- events/voiceStateUpdate.js
+++ events/voiceStateUpdate.js
@@ -14,6 +14,7 @@ module.exports = (client, oldmember, newmember) => {
         return client.vc[Math.floor(Math.random() * client.vc.length)];
     }
     function checkLatest(id) {
+        if(newmember.channel.members.size>1) {return;}
         var chanarray = []; const parnt = newmember.channel.parent
         parnt.children.each(channel => chanarray.push(channel.id))
         if(chanarray.includes(id)) {
