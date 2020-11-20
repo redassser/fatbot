df9fcca6c38b78fab242f3378be098125c5f43aa
redassser <redryan890@gmail.com>
2020-11-20 16:58:12 -0500

    fixed stupid mistake

--- events/voiceStateUpdate.js
+++ events/voiceStateUpdate.js
@@ -1,12 +1,12 @@
 const { VoiceBroadcast } = require("discord.js");
-
+const vcname = "auto-zone"
 module.exports = (client, oldmember, newmember) => {
     if(!newmember.guild.members.cache.get(client.user.id).hasPermission("MANAGE_CHANNELS")) {newmember.member.send("Sorry! I don't have the ``manage_messages`` permission!");return;}
-    if(newmember.channel!=null&&newmember.channel.parent.name=="auto-vc") { //if they join
+    if(newmember.channel!=null&&newmember.channel.parent.name==vcname) { //if they join
         var chanid = newmember.channel.id
         checkLatest(chanid);
     }
-    if(oldmember.channel!=null&&oldmember.channel.parent.name=="auto-vc") { //if they leave
+    if(oldmember.channel!=null&&oldmember.channel.parent.name==vcname) { //if they leave
         var chanid = oldmember.channel.id
         checkPast(chanid)
         return;
@@ -36,7 +36,7 @@ module.exports = (client, oldmember, newmember) => {
         .each(channel => emptychanarray.push(channel))
         if(emptychanarray.length>1) {
             xnum = emptychanarray.length-1
-        } else if (newmember.channel.parent.name=="auto-zone"&&emptychanarray.length===1) {
+        } else if (newmember.channel.parent.name==vcname&&emptychanarray.length===1) {
             xnum = emptychanarray.length
         }
         for(let x=0;x<xnum;x++) {
