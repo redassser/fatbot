50ee5fcc0226e41cdc9aed31b9b93c06a473853d
redassser <redryan890@gmail.com>
2021-12-29 00:16:56 -0500

    autorole patch

--- events/ready.js
+++ events/ready.js
@@ -9,4 +9,11 @@ module.exports = (client,message,array) => {
         status: 'online' 
     });
     //FRAMEWORK
+    //AUTOROLE
+    client.channels.cache
+        .filter(channel => (channel.name === "autorole"&&channel.type === "GUILD_TEXT"))
+        .forEach(channel => {
+            channel.messages.fetch({limit:20})
+        });
+    //AUTOROLE
 }
